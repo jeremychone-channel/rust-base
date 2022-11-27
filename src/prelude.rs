8b160013235931f9d7537f70841c942a9a4f9a9e
Jeremy Chone <jeremy.chone@gmail.com>
2022-11-26 19:15:56 -0800

    . update dependencies

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,7 +4,8 @@ pub use crate::error::Error;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-// Generic Wrapper tuple struct for newtype pattern, mostly for external type to type From/TryFrom conversions
+// Generic Wrapper tuple struct for newtype pattern,
+// mostly for external type to type From/TryFrom conversions
 pub struct W<T>(pub T);
 
 // Personal preference.
