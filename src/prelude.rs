3ddad3d81dca4154cd385fcfd961d7208cc57d12
Jeremy Chone <jeremy.chone@gmail.com>
2022-12-08 10:00:16 -0800

    . update dependencies

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,9 @@
 //! Crate prelude
 
+// Re-export the crate Error.
 pub use crate::error::Error;
 
+// Alias Result to be the crate Result.
 pub type Result<T> = core::result::Result<T, Error>;
 
 // Generic Wrapper tuple struct for newtype pattern,
--- src/utils/direntry_froms.rs
+++ src/utils/direntry_froms.rs
@@ -8,11 +8,6 @@ impl TryFrom<W<&DirEntry>> for String {
 			.path()
 			.to_str()
 			.map(String::from)
-			.ok_or_else(|| {
-				Error::Generic(f!(
-					"Invalid path {:?}",
-					val.0
-				))
-			})
+			.ok_or_else(|| Error::Generic(f!("Invalid path {:?}", val.0)))
 	}
 }
